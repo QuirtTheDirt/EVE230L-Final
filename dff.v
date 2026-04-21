879b5d4c66753cada52719c20b86649bb8c6c7e9
GabrielSJones <35454714+GabrielSJones@users.noreply.github.com>
2026-04-21 16:50:25 -0600

    dff

---
+++ dff.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/21/2026 04:49:06 PM
+// Design Name: 
+// Module Name: dff
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dff(
+
+    );
+endmodule
